dcc1ad403fa1f36621ba35683625a287f10dcfc2
AngusKo <anan20.tw@gmail.com>
2020-05-26 17:43:42 +0800

    test

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function encodeImageFileAsURL(element) {
             url: 'https://automl.googleapis.com/v1/projects/923293844511/locations/us-central1/models/IOD5545052643153412096:predict',
             contentType: 'application/json; charset=utf-8',
             data: imgData,
-            headers: {'Authorization': 'Bearer $(gcloud auth application-default print-access-token)'},
+            headers: {'Authorization: Bearer $(gcloud auth application-default print-access-token)'},
             success: function (msg) {
                 console.log(msg);
             },
